0140e1a5348298fbfdbdbec27212d967090cc026
tusherikbal <64355570+tusherikbal@users.noreply.github.com>
2022-11-12 18:32:01 +0600

    Add files via upload

---
+++ wp-haleem-child/functions.php
@@ -0,0 +1,11 @@
+
+<?php
+ 
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( 'parenthandle' ), 
+        wp_get_theme()->get('Version') // this only works if you have Version in the style header
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
